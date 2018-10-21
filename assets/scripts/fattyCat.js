4b0e3fc45560a16522ad1307fe9c7261a7323ed4
Maciej Kozlowski <mjp.kozlowski@gmail.com>
2018-10-21 15:57:39 +0200

    Bugfixes and further refactoring

--- assets/scripts/fattyCat.js
+++ assets/scripts/fattyCat.js
@@ -47,8 +47,8 @@ var spacebar;
 
 var score = 0;
 var highScore = 0;
-var savedScore;
-var localStorageName = "high_score";
+var fattyCatSavedScore;
+var localStorageName = "fatty_cat_high_score";
 
 var meowCounter = 0; //meows
 
@@ -81,9 +81,225 @@ function preload () {
     enableCrispRendering();
 }
 
+
+
+function create () {
+    adjustGameScale();
+    initializeAudio();
+    loadHighScore();
+    displayHighScore();
+    displayScore();
+    displayTitleText();
+    createCat();
+    createClouds();
+    createBarriers();
+    initializeInput();
+
+}
+
+
+
+function update () {
+    handleClouds();
+    handleBarriers();
+    handleCollisions();
+    bringThingsToTop();
+}
+
+/***********\
+| FUNCTIONS |
+\***********/
+
+function loadAssets () {
+    game.load.image("Tesla", "assets/sprites/tesla2.png");
+    game.load.image("Barrier", "assets/sprites/barrier.png");
+    game.load.image("Cloud", "assets/sprites/cloud.png");
+    game.load.audio("meow1", ["assets/sounds/meow1.mp3", "assets/sounds/meow1.ogg"]);
+    game.load.audio("meow2", ["assets/sounds/meow2.mp3", "assets/sounds/meow2.ogg"]);
+    game.load.audio("meow3", ["assets/sounds/meow3.mp3", "assets/sounds/meow3.ogg"]);
+    game.load.audio("meow4", ["assets/sounds/meow4.mp3", "assets/sounds/meow4.ogg"]);
+    game.load.audio("meow5", ["assets/sounds/meow5.mp3", "assets/sounds/meow5.ogg"]);
+    game.load.audio("meow6", ["assets/sounds/meow6.mp3", "assets/sounds/meow6.ogg"]);
+    game.load.audio("flap", ["assets/sounds/Jump9.mp3", "assets/sounds/Jump9.ogg"]);
+    game.load.audio("hit", ["assets/sounds/Hit_Hurt2.mp3", "assets/sounds/Hit_Hurt2.ogg"]);
+    game.load.bitmapFont("font", "assets/fonts/font.png", "assets/fonts/font.fnt");
+}
+
+function enableCrispRendering () {
+    game.renderer.renderSession.roundPixels = true;
+	Phaser.Canvas.setImageRenderingCrisp(game.canvas);
+	game.stage.backgroundColor = "#00ccff";
+}
+
+function adjustGameScale () {
+	game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+}
+
+function initializeAudio () {
+    meowSounds = [
+        game.add.audio("meow1"),
+        game.add.audio("meow2"),
+        game.add.audio("meow3"),
+        game.add.audio("meow4"),
+        game.add.audio("meow5"),
+        game.add.audio("meow6"),
+    ];
+
+    hitSound = game.add.audio("hit");
+    flapSound = game.add.audio("flap");
+}
+
+function loadHighScore () {
+    fattyCatSavedScore = localStorage.getItem(localStorageName) == null ? {score: 0} : JSON.parse(localStorage.getItem(localStorageName));
+}
+
+function displayHighScore () {
+    highScoreDisplay = game.add.bitmapText(game.width / 2, 100, "font", fattyCatSavedScore.score.toString(), 72);
+    highScoreDisplay.tint = HIGH_SCORE_COLOR;
+    highScoreDisplay.anchor.setTo(0.5);
+}
+
+function displayScore () {
+    scoreDisplay = game.add.bitmapText(game.width / 2, 30, "font", "0", 72);
+    scoreDisplay.anchor.setTo(0.5);
+}
+
+function displayTitleText () {
+    titleText.instructions = game.add.bitmapText(50, 670, "font", "LMB, space or tap", 32);
+    titleText.instructions.tint = CREDITS_COLOR;
+
+    titleText.credits = game.add.bitmapText(60, 710, "font", "gfx, snd & prg: matzieq", 24);
+    titleText.credits.tint = CREDITS_COLOR;
+
+    titleText.disclaimer = game.add.bitmapText(50, 750, "font", "image of my cat used with permission", 24);
+    titleText.disclaimer.tint = CREDITS_COLOR;
+}
+
+function createCat () {
+    tesla = game.add.sprite(CAT_START_X, CAT_START_Y, "Tesla");
+    tesla.anchor.setTo(0.5);
+    tesla.scale.setTo(CAT_SCALE);
+
+}
+
+function createClouds () {
+    clouds = game.add.group();
+    cloudTimer = game.rnd.between(10, 60);
+}
+
 function createBarriers () {
     barriers.topBarriers = game.add.group();
     barriers.bottomBarriers = game.add.group();
+}
+
+function initializeInput () {
+    spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    resetInputState();
+
+}
+
+function resetInputState () {
+    game.input.onDown.dispose();
+    spacebar.onDown.dispose();
+
+    game.input.onDown.add(fsm, this); // mouse or touch
+    spacebar.onDown.add(fsm, this);
+}
+
+function startGame () {
+    barrierCounter = BARRIER_FREQUENCY;
+    enableCatPhysics();
+    disableCredits();
+    fsm = flap;
+    gameOn = true;
+    resetInputState();
+    rotateTowardsBottom();
+    flap();
+}
+
+function restartGame () {
+    if (tesla.y > SCREEN_HEIGHT) {
+        hitRotation.stop();
+        tesla.reset(CAT_START_X, CAT_START_Y);
+        barriers.topBarriers.forEach(function(item) {
+            item.kill();
+            item.x = 600;
+        });
+        barriers.bottomBarriers.forEach(function(item) {
+            item.kill();
+            item.x = 600;
+        });
+        clouds.forEach(function(item) {
+            item.kill();
+            item.x = 600;
+        });
+        startGame();
+    }
+}
+
+function enableCatPhysics () {  
+    game.physics.enable(tesla, Phaser.Physics.ARCADE);
+    tesla.body.setSize(100, 40, 10, 7);
+    tesla.body.collideWorldBounds = true;
+    tesla.body.gravity.y = GRAVITY;
+
+}
+
+function disableCredits () {
+    for (var key in titleText) {
+        titleText[key].visible = false;
+    }
+}
+
+function flap () {
+    if(gameOn) {
+        flapSound.play();
+        tesla.body.velocity.y = JUMP_STRENGTH; //just some vertical velocity to counteract gravity
+        tesla.angle = -50;
+        rotateTowardsBottom();
+    }
+}
+
+function addCloud () {
+    cloudTimer = game.rnd.between(10, 30);
+
+    var cloud = clouds.getFirstExists(false);
+
+    if (!cloud) {
+        cloud = game.add.sprite(0, 0, "Cloud");
+    }
+    cloud.reset (SCREEN_WIDTH * 1.5, game.rnd.between(10, 750));
+
+    game.physics.enable(cloud, Phaser.Physics.ARCADE);
+    cloud.anchor.setTo(1, 0);
+    var cloudSize = game.rnd.realInRange(0.6, 1.5);
+    cloud.scale.setTo(cloudSize);
+    cloud.body.velocity.x = -cloudSize * 100 - 200;
+    cloud.sendToBack();
+    clouds.add(cloud);
+}
+
+function handleClouds () {
+    if (gameOn) {
+        cloudTimer--;
+        if(cloudTimer <= 0) {
+            addCloud();
+        }
+    }
+
+    clouds.forEach(function (cloud) {
+        if (cloud.x < 0) {
+            cloud.kill(); //remove invisible clouds
+        }
+    });
+}
+
+function rotateTowardsBottom() {
+    downRotation = game.add.tween(tesla).to({ //diving tween
+            angle: 50
+    }, ROTATION_SPEED, "Linear", true);
 }
 
 function handleBarriers () {
@@ -157,20 +373,6 @@ function createBottomBarrier (posY) {
 
 }
 
-function create () {
-    adjustGameScale();
-    initializeAudio();
-    loadHighScore();
-    displayHighScore();
-    displayScore();
-    displayTitleText();
-    createCat();
-    createClouds();
-    createBarriers();
-    initializeInput();
-
-}
-
 function handleCollisions () {
     if (gameOn) {
         highScoreDisplay.text = "";
@@ -207,7 +409,7 @@ function hitSomething () {
 }
 
 function saveHighScore () {
-    highScore = Math.max(score, savedScore.score);
+    highScore = Math.max(score, fattyCatSavedScore.score);
     localStorage.setItem(localStorageName, JSON.stringify({
         score: highScore
     }));
@@ -222,210 +424,4 @@ function stopMoving (group) {
 
 function bringThingsToTop () {
     tesla.bringToTop();
-    scoreDisplay.bringToTop();
-    highScoreDisplay.bringToTop();
-}
-
-function update () {
-    handleClouds();
-    handleBarriers();
-    handleCollisions();
-    bringThingsToTop();
-}
-
-function loadAssets () {
-    game.load.image("Tesla", "assets/sprites/tesla2.png");
-    game.load.image("Barrier", "assets/sprites/barrier.png");
-    game.load.image("Cloud", "assets/sprites/cloud.png");
-    game.load.audio("meow1", ["assets/sounds/meow1.mp3", "assets/sounds/meow1.ogg"]);
-    game.load.audio("meow2", ["assets/sounds/meow2.mp3", "assets/sounds/meow2.ogg"]);
-    game.load.audio("meow3", ["assets/sounds/meow3.mp3", "assets/sounds/meow3.ogg"]);
-    game.load.audio("meow4", ["assets/sounds/meow4.mp3", "assets/sounds/meow4.ogg"]);
-    game.load.audio("meow5", ["assets/sounds/meow5.mp3", "assets/sounds/meow5.ogg"]);
-    game.load.audio("meow6", ["assets/sounds/meow6.mp3", "assets/sounds/meow6.ogg"]);
-    game.load.audio("flap", ["assets/sounds/Jump9.mp3", "assets/sounds/Jump9.ogg"]);
-    game.load.audio("hit", ["assets/sounds/Hit_Hurt2.mp3", "assets/sounds/Hit_Hurt2.ogg"]);
-    game.load.bitmapFont("font", "assets/fonts/font.png", "assets/fonts/font.fnt");
-}
-
-function enableCrispRendering () {
-    game.renderer.renderSession.roundPixels = true;
-	Phaser.Canvas.setImageRenderingCrisp(game.canvas);
-	game.stage.backgroundColor = "#00ccff";
-}
-
-function adjustGameScale () {
-	game.scale.pageAlignHorizontally = true;
-    game.scale.pageAlignVertically = true;
-	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-}
-
-function initializeAudio () {
-    meowSounds = [
-        game.add.audio("meow1"),
-        game.add.audio("meow2"),
-        game.add.audio("meow3"),
-        game.add.audio("meow4"),
-        game.add.audio("meow5"),
-        game.add.audio("meow6"),
-    ];
-
-    hitSound = game.add.audio("hit");
-    flapSound = game.add.audio("flap");
-}
-
-function loadHighScore () {
-    savedScore = localStorage.getItem(localStorageName) == null ? {score: 0} : JSON.parse(localStorage.getItem(localStorageName));
-}
-
-function displayHighScore () {
-    highScoreDisplay = game.add.bitmapText(game.width / 2, 100, "font", savedScore.score.toString(), 72);
-    highScoreDisplay.tint = HIGH_SCORE_COLOR;
-    highScoreDisplay.anchor.setTo(0.5);
-}
-
-function displayScore () {
-    scoreDisplay = game.add.bitmapText(game.width / 2, 30, "font", "0", 72);
-    scoreDisplay.anchor.setTo(0.5);
-}
-
-function displayTitleText () {
-    titleText.instructions = game.add.bitmapText(50, 670, "font", "LMB, space or tap", 32);
-    titleText.instructions.tint = CREDITS_COLOR;
-
-    titleText.credits = game.add.bitmapText(60, 710, "font", "gfx, snd & prg: matzieq", 24);
-    titleText.credits.tint = CREDITS_COLOR;
-
-    titleText.disclaimer = game.add.bitmapText(50, 750, "font", "image of my cat used with permission", 24);
-    titleText.disclaimer.tint = CREDITS_COLOR;
-}
-
-function createCat () {
-    tesla = game.add.sprite(CAT_START_X, CAT_START_Y, "Tesla");
-    tesla.anchor.setTo(0.5);
-    tesla.scale.setTo(CAT_SCALE);
-
-}
-
-function initializeInput () {
-    spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    resetInputState();
-
-}
-
-function resetInputState () {
-    game.input.onDown.dispose();
-    spacebar.onDown.dispose();
-
-    game.input.onDown.add(fsm, this); // mouse or touch
-    spacebar.onDown.add(fsm, this);
-}
-
-function startGame () {
-    barrierCounter = BARRIER_FREQUENCY;
-    enableCatPhysics();
-    flipCreditsVisibility();
-    fsm = flap;
-    gameOn = true;
-    resetInputState();
-    rotateTowardsBottom();
-    flap();
-}
-
-function restartGame () {
-    if (tesla.y > SCREEN_HEIGHT) {
-        hitRotation.stop();
-        tesla.reset(CAT_START_X, CAT_START_Y);
-        barriers.topBarriers.forEach(function(item) {
-            item.kill();
-            item.x = 600;
-        });
-        barriers.bottomBarriers.forEach(function(item) {
-            item.kill();
-            item.x = 600;
-        });
-        clouds.forEach(function(item) {
-            item.kill();
-            item.x = 600;
-        });
-        startGame();
-    }
-}
-
-function enableCatPhysics () {  
-    game.physics.enable(tesla, Phaser.Physics.ARCADE);
-    tesla.body.setSize(100, 40, 10, 7);
-    tesla.body.collideWorldBounds = true;
-    tesla.body.gravity.y = GRAVITY;
-
-}
-
-function flipCreditsVisibility () {
-    for (var key in titleText) {
-        titleText[key].visible = !titleText[key].visible;
-    }
-}
-
-function flap () {
-    if(gameOn) {
-        flapSound.play();
-        tesla.body.velocity.y = JUMP_STRENGTH; //just some vertical velocity to counteract gravity
-        tesla.angle = -50;
-        rotateTowardsBottom();
-    } else {
-        // if (tesla.y > 800) {
-        //     gameOn = true;
-        //     score = 0;
-        //     placeBarrier = true;
-        //     barrierCounter = BARRIER_FREQUENCY;
-        //     this.state.start(this.state.current); 
-        //}
-    }
-    console.log("Flap!");
-}
-
-
-function createClouds () {
-    clouds = game.add.group();
-    cloudTimer = game.rnd.between(10, 60);
-}
-
-function addCloud () {
-    cloudTimer = game.rnd.between(10, 30);
-
-    var cloud = clouds.getFirstExists(false);
-
-    if (!cloud) {
-        cloud = game.add.sprite(0, 0, "Cloud");
-    }
-    cloud.reset (SCREEN_WIDTH * 1.5, game.rnd.between(10, 750));
-
-    game.physics.enable(cloud, Phaser.Physics.ARCADE);
-    cloud.anchor.setTo(1, 0);
-    var cloudSize = game.rnd.realInRange(0.6, 1.5);
-    cloud.scale.setTo(cloudSize);
-    cloud.body.velocity.x = -cloudSize * 100 - 200;
-    cloud.sendToBack();
-    clouds.add(cloud);
-}
-
-function handleClouds () {
-    if (gameOn) {
-        cloudTimer--;
-        if(cloudTimer <= 0) {
-            addCloud();
-        }
-    }
-
-    clouds.forEach(function (cloud) {
-        if (cloud.x < 0) {
-            cloud.kill(); //remove invisible clouds
-        }
-    });
-}
-
-function rotateTowardsBottom() {
-    downRotation = game.add.tween(tesla).to({ //diving tween
-            angle: 50
-    }, ROTATION_SPEED, "Linear", true);
 }
